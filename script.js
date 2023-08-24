81ca9ef65c03d0b864a271e6960aa365f926aa54
Dani Gomez <d.gomezmartinez@icloud.com>
2023-08-24 18:38:13 +0200

    Round
    Function to play one round

--- script.js
+++ script.js
@@ -12,3 +12,48 @@ let getComputerChoice = () => {
 
     return choice;
 }
+
+let playRound = (playerChoice, ComputerChoice) => {
+    switch (ComputerChoice) {
+        case 0: // Computer choice Rock
+            if (playerChoice == 'rock') {
+                console.log('Tie');
+            } else if (playerChoice == 'paper') {
+                playerScore++;
+                console.log('You Win this round');
+            } else if (playerChoice == 'scissor') {
+                computerScore++;
+                console.log('computer Win this round');
+            } else {
+                console.log('You have entered a wrong value, invalid round.');
+            }
+            break;
+        case 1: // Computer choice Paper
+            if (playerChoice == 'rock') {
+                computerScore++;
+                console.log('computer Win this round');
+            } else if (playerChoice == 'paper') {
+                console.log('Tie');
+            } else if (playerChoice == 'scissor') {
+                playerScore++;
+                console.log('You Win this round');
+            } else {
+                console.log('You have entered a wrong value, invalid round.');
+            }
+            break;
+        case 2: // Computer choice Scissor
+            if (playerChoice == 'rock') {
+                playerScore++;
+                console.log('You Win this round');
+            } else if (playerChoice == 'paper') {
+                computerScore++;
+                console.log('computer Win this round');
+            } else if (playerChoice == 'scissor') {
+                console.log('Tie');
+            } else {
+                console.log('You have entered a wrong value, invalid round.');
+            }
+            break; 
+    }
+
+}
